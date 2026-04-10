f5c893ed3e148b07d2272010abc82687b6299816
RomainAuthier <romainauthier@outlook.com>
2026-04-10 13:58:18 +0200

    Modif CORS

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/users", usersRouter);
 app.use("/surprises", surprisesRouter);
